d0905fe3a89c081aa68ac6007fbf61bcf44e96db
300007814 <nishant.takkar@myntra.com>
2018-06-15 04:46:11 +0530

    Added twitter and insta integration

--- src/main/java/com/myntra/critico/controller/ProductController.java
+++ src/main/java/com/myntra/critico/controller/ProductController.java
@@ -1,13 +1,20 @@
 package com.myntra.critico.controller;
 
 import com.myntra.critico.exception.ResourceNotFoundException;
+import com.myntra.critico.manager.InstagramManager;
 import com.myntra.critico.model.Product;
+import com.myntra.critico.model.Review;
 import com.myntra.critico.repository.ProductRepository;
+import com.myntra.critico.repository.ReviewRepository;
+import org.jinstagram.entity.users.basicinfo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import twitter4j.TwitterException;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -15,6 +22,12 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    InstagramManager instagramManager;
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
@@ -31,6 +44,54 @@ public class ProductController {
         return productRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "id", productId));
     }
+
+    @RequestMapping(value = "/twitter",method = RequestMethod.GET)
+    public @ResponseBody void getOAuthForTwitter(@RequestParam("oauth_token") String token,@RequestParam("oauth_verifier") String verifier) {
+        System.out.print("abc");
+        try {
+            instagramManager.processTwitterData(token,verifier);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "/twitter/oauth",method = RequestMethod.GET)
+    public @ResponseBody String getTwitterDetails() {
+        System.out.print("abc");
+        try {
+           return instagramManager.getOAuth();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/socialreview/{id}",method = RequestMethod.GET)
+    public @ResponseBody List<Review> getSocialReviews(@PathVariable(value = "id") Long productId) {
+
+        Optional<Product> product = productRepository.findById(productId);
+        if(!product.isPresent()){
+            return null;
+        }
+        try {
+            List<Review> twitterReviews = instagramManager.getTwitterReviews(product.get());
+            for(Review review:twitterReviews){
+                try {
+                    reviewRepository.save(review);
+                }catch (Exception e){ }
+            }
+            return twitterReviews;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 
     /*
     @PutMapping("/products/{id}")
---
+++ src/main/java/com/myntra/critico/manager/InstagramManager.java
@@ -0,0 +1,112 @@
+package com.myntra.critico.manager;
+
+import com.myntra.critico.model.Product;
+import com.myntra.critico.model.Review;
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.InstagramAuthService;
+import org.jinstagram.auth.model.Token;
+import org.jinstagram.auth.model.Verifier;
+import org.jinstagram.auth.oauth.InstagramService;
+import org.jinstagram.entity.users.basicinfo.UserInfo;
+import org.jinstagram.entity.users.feed.MediaFeed;
+import org.jinstagram.entity.users.feed.MediaFeedData;
+import org.jinstagram.entity.users.feed.UserFeed;
+import org.jinstagram.entity.users.feed.UserFeedData;
+import org.jinstagram.exceptions.InstagramException;
+import org.springframework.stereotype.Service;
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class InstagramManager {
+
+    Twitter twitter;
+    RequestToken requestToken;
+
+    public UserInfo processInstaCode(String code) {
+
+        InstagramService service = new InstagramAuthService()
+                .apiKey("736dd2369b5d42058a04925bbba80dda")
+                .apiSecret("5134d1d6f1b148948d865a31385dae6c")
+                .callback("http://localhost:9090/api/instacode")
+                .scope("follower_list")
+                .build();
+
+        Verifier verifier = new Verifier(code);
+        Token accessToken = service.getAccessToken(verifier);
+        Instagram instagram = new Instagram(accessToken);
+        try {
+            UserInfo currentUserInfo = instagram.getCurrentUserInfo();
+            UserFeed userFollowedByList = instagram.getUserFollowList(currentUserInfo.getData().getId());
+
+            for (UserFeedData userFeedData : userFollowedByList.getUserList()) {
+                MediaFeed recentMediaFeed = instagram.getRecentMediaFeed(userFeedData.getId());
+                for (MediaFeedData mediaFeedData : recentMediaFeed.getData()) {
+                    for (String tag : mediaFeedData.getTags()) {
+                        if (tag.equals("xyz")) {
+                            System.out.print("Do something");
+                        }
+                    }
+
+                }
+
+            }
+
+            return currentUserInfo;
+
+        } catch (InstagramException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public void processTwitterData(String token, String verifier) throws TwitterException, IOException {
+
+        AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
+        twitter.setOAuthAccessToken(accessToken);
+
+    }
+
+    public List<Review> getTwitterReviews(Product product) throws TwitterException {
+
+        List<Review> reviews = new ArrayList<>();
+        PagableResponseList<User> friendsList = twitter.getFriendsList(twitter.getId(), -10);
+        for (int i = 0; i < friendsList.size(); i++) {
+            User user = friendsList.get(i);
+
+            ResponseList<Status> userTimeline = twitter.getUserTimeline(user.getId(), new Paging(1, 10));
+            String testword = product.getProductName();
+            if (Objects.nonNull(userTimeline) && !userTimeline.isEmpty()) {
+                for (Status status : userTimeline) {
+                    if (status.getText().contains(testword)) {
+                        Review review = new Review();
+                        review.setReviewerName(user.getName());
+                        review.setReviewContent(status.getText());
+                        review.setSourceName("Twitter");
+                        review.setCreatedAt(status.getCreatedAt());
+                        review.setReviewerThumbnailImgUrl(user.getProfileImageURL());
+                        reviews.add(review);
+                    }
+                }
+            }
+        }
+        return reviews;
+    }
+
+    public String getOAuth() throws TwitterException, IOException {
+
+        twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("73RkVGYmdjjTcKM8NAfZmC6SW", "8GIuHRiWgk7i7nd8KaweLOno5hN4ZH2QJAaUBvL3FnfAIziCEs");
+        requestToken = twitter.getOAuthRequestToken();
+        return requestToken.getAuthorizationURL();
+    }
+
+
+}
